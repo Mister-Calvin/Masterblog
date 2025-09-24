d3f38ba6cdd0cf49c5b5f9d92a527078ccdc8a0c
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-24 08:09:36 +0200

    Added blog post JSON loading and template placeholders to index route

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+import json
+
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=load_data("blog_posts.json"))
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
