09782f544e783f4fda79c89502ed52c1321d0b35
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-25 20:34:28 +0200

    Add custom error handlers and templates for 404 and 500 errors

--- app.py
+++ app.py
@@ -112,6 +112,14 @@ def likes(post_id):
 
     return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
 
 
 
